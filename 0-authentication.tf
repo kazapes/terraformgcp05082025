44299b518c75e0d574662cc7917854e6e23dffb7
kazapes <cassiem.davids316@gmail.com>
2025-05-08 01:38:35 -0400

    second adding terraform files commit

---
+++ 0-authentication.tf
@@ -0,0 +1,11 @@
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs
+provider "google" {
+  project = "kazapes1"
+  region  = "us-east1"
+  credentials = "kazapes1-d5061cf53f9a.json"
+}
+
+# I'm doing this tonight while prepping my students for their sessions with Lizzo
+
+# comment there will be no diddy oil bootygasms here
+# Lizzo will be pegging Carlton and inducing a bootygasm deep in him with Chewbacca watching!
---
+++ 1-backend.tf
@@ -0,0 +1,14 @@
+# https://www.terraform.io/language/settings/backends/gcs
+terraform {
+  backend "gcs" {
+    bucket = "sundaymorning0502025"
+    prefix = "terraform/state"
+    credentials = "kazapes1-d5061cf53f9a.json"
+  }
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
+}
